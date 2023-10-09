40e57cf3fc48ca3fea3bfc1eff5743bd463864d0
burillu <burillu90@hotmail.it>
2023-10-09 15:40:41 +0200

    add variables and conditions.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-const unitPrice= 0.21;
+const unitPrice = 0.21;
+let tripKm = parseInt(prompt('Inserisci qui il numero dei chilometri da percorrere per il tuo viaggio'));
+let age = parseInt(prompt('Inserisci qui la tua età'));
+let finalPrice;
+
+if (age > 65 && age < 110){
+    finalPrice =(unitPrice * tripKm)* 0.6;
+    
+} else if (age < 18 && age > 0){
+    finalPrice = (unitPrice * tripKm)* 0.8;
+    
+} else if (age> 17 && age <= 65){
+    finalPrice =(unitPrice * tripKm);
+}
+else {
+    console.log('Dati inseriti non correttamente. Inserire chilometri e età con valori numerici e non negativi');
+}
+if( finalPrice){
+    console.log('Il costo del biglietto è: €', finalPrice);
+}
